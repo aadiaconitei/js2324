5e06f85435a7a54267a054904076c1665c2f374c
aadiaconitei <aadiaconitei@yahoo.com>
2024-01-31 21:00:16 +0200

    curs8+9

--- curs8/aplicatii/js/jsap1.js
+++ curs8/aplicatii/js/jsap1.js
@@ -1,7 +1,9 @@
+let r = 10;
 let suma = [8, 2, 3].reduce((r, x) => r + x);
 console.log(suma);
 
 const myArr = [1, 2, 3, 4];
-// operatorul spread: ... - ne arata ca variabila ce urmeaza dupa acest operator este un array
-let newArr = [5, 6, 7, 8, ...myArr];
+// let newArr = myArr.concat([9, 7, 5]);
+// // operatorul spread: ... - ne arata ca variabila ce urmeaza dupa acest operator este un array
+let newArr = [5, 6, 7, 8, myArr];
 console.log(newArr);
